587faf551eae36c6214c4deb437f459a06ea4a2a
Jonathan Na <jna6@hawaii.edu>
2019-09-18 13:53:46 -0800

    started classes

--- script5.js
+++ script5.js
@@ -7,3 +7,28 @@ var world = function() {
   return "Hello World!";
 }
 console.log(world());
+//arrow with parameters:
+var hello2 = (val, val2) => "Hello " + val + val2
+console.log(hello2('aholes', ' piece of shet'));
+//Classes
+class Car {
+  constructor(brand) {
+    Car.carname = brand;
+  }
+  present(x) {
+    return x + ", I have a " + Car.carname;
+  }
+}
+var mycar = new Car("Ford");
+console.log(mycar.present("Hello"));
+//OR, notice how the this replaces the Car class
+class Car2 {
+  constructor(brand) {
+    this.carname = brand;
+  }
+  present(x) {
+    return x + ", I have a " + this.carname;
+  }
+}
+var mycar2 = new Car2("toyota");
+console.log(mycar2.present("Hello"));
