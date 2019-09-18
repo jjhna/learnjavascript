62a8c4658d42031864dec16388f176c496fe07f2
Jonathan Na <jna6@hawaii.edu>
2019-09-17 18:19:54 -0800

    hoisting

---
+++ script4.js
@@ -0,0 +1,14 @@
+console.log("john".constructor);
+
+var x = 5 | 1;
+console.log(x);
+
+//Javascript declarations can be hoisted, meaning variables don't need to be declared at the top
+a = 20;
+console.log(a);
+var a;
+console.log(a);
+a = [2, 3];
+console.log(a[0] + " " + a[1]);
+
+//Use strict
